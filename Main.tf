86aa1119f2ed93ad93e8594793ee3de8a43903af
tehzeeb45 <joyiahassan95@gmail.com>
2024-10-11 00:31:28 -0700

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -1,59 +1,79 @@
-#!/bin/bash
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "XXXXX"      
+  secret_key = "XXXXXX"  
+}
 
-# Update the package list
-sudo apt update
+# Security Groups
+resource "aws_security_group" "First_security_group" { # ya (First_security_group)  reference karne ke liye hota hai (jaise kisi EC2 instance ke saath jorna).
+  name = "Test1SecurityGroup"
+  
+#####  Ingress Rules
+###    These rules control incoming traffic to your instances:
 
-# Install Nginx, Git, and OpenJDK
-sudo apt install nginx git openjdk-21-jdk -y
+  # Allow HTTP on port 80
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] ###   means that traffic from any IP address can access this port.
+  }
 
-# Verify Java installation
-java --version
+  # Allow SSH on port 22
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-# Install Jenkins
-sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
-echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt update
-sudo apt install jenkins -y
+  # Allow Jenkins on port 8080
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-# Install Docker
-sudo apt install ca-certificates curl -y
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# Add the Docker repository to Apt sources
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  # Allow custom container port (e.g., for an application running on 4440)
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-# Update package list and install Docker components
-sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
-# Install Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/v2.29.7/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+##  Egress Rule
+##   This rule controls outgoing traffic from your instances:
 
-# Make Docker Compose executable
-sudo chmod +x /usr/local/bin/docker-compose
 
-# Verify Docker Compose installation
-docker-compose --version
+  # Allow all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  ## -1 means All protocol 
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+}
 
-curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
-    sudo apt-get install -y nodejs
-   # Install PM2 (Process Manager for Node.js)
-    sudo npm install -g pm2
-# Install MongoDB
-sudo apt-get install gnupg curl
-curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | \
-   sudo gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg --dearmor
+# AWS Instance
+resource "aws_instance" "First_instance" {
+  ami                         = "ami-0e86e20dae9224db8" # Ubuntu
+  instance_type               = "t2.medium"
+  key_name                    = "XXXXXXXX"
+  vpc_security_group_ids      = [aws_security_group.First_security_group.id]
+  user_data = file("${path.module}/user-data.sh")
 
-# Add MongoDB repository
-echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] https://repo.mongodb.org/apt/ubuntu noble/mongodb-org/8.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list
+ 
 
-# Update package list and install MongoDB
-sudo apt-get update
-sudo apt-get install -y mongodb-org
-sudo systemctl start mongod
-sudo systemctl enable mongod
+  tags = {
+    Name = "instance_from_terraform"
+  }
+}
+
+# Output the public IP of the created instance
+output "instance_public_ip" {
+  value = aws_instance.First_instance.public_ip
+}
